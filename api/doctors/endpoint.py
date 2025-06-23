09fbad3e0419223f0b3ae349f9e70b5f124732c5
Maria Laura <Maria.Laura.Bisaccia1@ibm.com>
2025-06-23 17:45:50 -0300

    fix: limit create for admin role only

--- api/doctors/endpoint.py
+++ api/doctors/endpoint.py
@@ -8,6 +8,7 @@ from api.core.database import get_db
 from sqlalchemy.orm import Session
 from api.core.models import Doctor 
 from api.core.models import Doctor as DoctorModel
+from api.auth.endpoint import get_current_admin
 
 
 
@@ -24,8 +25,9 @@ class DoctorRead(BaseModel):
 def get_all_doctors(db: Session = Depends(get_db)):
     return db.query(DoctorModel).all()
 
-@router.post("/doctors/", response_model=DoctorRead)
-def create_doctor(doctor: DoctorCreate, db: Session = Depends(get_db)):
+#aca limito solo el post para el admin
+@router.post("", response_model=DoctorRead)
+def create_doctor(doctor: DoctorCreate, db: Session = Depends(get_db), user = Depends(get_current_admin)):
     new_doctor = Doctor(id=str(uuid.uuid4()), name=doctor.name)
     db.add(new_doctor)
     db.commit()
